2c9c57e86fcb72696fadf6d53458d86003c25886
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-05-30 15:29:47 -0400

    intial commit

---
+++ routes/dalleRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import { OpenAI } from 'openai'
+
+dotenv.config();
+
+const router = express.Router();
+
+const openaiAPI = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+router.route('/').get((req, res) => {
+    res.send('Hello from DALL-E');
+})
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openaiAPI.images.generate({
+            model: 'dall-e-3',
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        })
+
+        const image = aiResponse.data[0].b64_json;
+
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.error.message);
+    }
+})
+
+export default router;
